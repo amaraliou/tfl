ecdab38ceb98a5026144be4c8b3d5287dacbe614
Aliou Amar <amarraliou97@gmail.com>
2020-06-24 21:58:01 +0100

    Client getters

--- client.go
+++ client.go
@@ -1,13 +1,39 @@
 package tfl
 
+import (
+	"net/http"
+	"time"
+)
+
 // Client ...
 type Client struct {
-	AppID     string
-	AppSecret string
-	// To add other fields
+	appID     string
+	appSecret string
+	baseURL   string
+	client    *http.Client
 }
 
 // NewClient ...
-func NewClient(appID, appSecret string) (*Client, error) {
-	return &Client{}, nil
+func NewClient(appID, appSecret, baseURL string) (*Client, error) {
+	return &Client{
+		appID:     appID,
+		appSecret: appSecret,
+		baseURL:   baseURL,
+		client:    &http.Client{Timeout: 300 * time.Second},
+	}, nil
+}
+
+// GetAppID ...
+func (client *Client) GetAppID() string {
+	return client.appID
+}
+
+// GetAppSecret ...
+func (client *Client) GetAppSecret() string {
+	return client.appSecret
+}
+
+// GetBaseURL ...
+func (client *Client) GetBaseURL() string {
+	return client.baseURL
 }
